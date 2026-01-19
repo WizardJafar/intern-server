3629361398a8c8cc651c777a8f2ceff91c4cec7f
WizardJafar <sigmajafarbek@gmail.com>
2026-01-19 22:40:40 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://int-booking.vercel.app",
       "https://mentors-rho.vercel.app",
       "https://interns-lovat.vercel.app",
       "https://internship-admin-zeta.vercel.app",
